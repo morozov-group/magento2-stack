9f23272a66928e483e8adfed2e9ee72ead989c8e
Kirill Morozov <kir.morozov@gmail.com>
2017-07-26 21:20:56 +0300

    Removed useless creation

--- init.sh
+++ init.sh
@@ -3,9 +3,6 @@
 curl -o ./env/nginx.conf.d/nginx.conf.sample https://raw.githubusercontent.com/magento/magento2/develop/nginx.conf.sample
 
 mkdir -p ./www/html
-mkdir -p www/m2/test.m2/current/pub/
-echo "hello" > www/m2/test.m2/current/pub/index.php
-
 
 ln -sfn ./test.m2 ./www/m2/$(curl ifconfig.co)
 ln -sfn ./test.m2 ./www/m2/nginx
